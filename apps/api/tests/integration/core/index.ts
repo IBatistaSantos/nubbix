887025e4e8f4dac27bb0724dc85f9c255a60d706
Israel Batista <israelbatiista19@gmail.com>
2025-12-28 21:17:56 -0300

    chore: remove integration tests and CI workflow step
    - Remove all integration test files and directories
    - Remove test-integration job from CI workflow
    - Remove test:integration script from package.json

--- apps/api/tests/integration/core/index.ts
+++
@@ -1,4 +0,0 @@
-export { UseCaseTester } from "./useCaseTester";
-export { QueryHelpers } from "./queryHelpers";
-export { TestRequestBuilder } from "./testBuilder";
-export { TestAssertions } from "./assertions";
